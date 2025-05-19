7e13928578da8973577f4b1dba2c88fd0a2813e7
mferraretto <matheus.ferraretto94@gmail.com>
2025-05-19 08:47:33 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,19 @@
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('precificador-cache-v1').then(cache => {
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('precificador-cache').then(cache => {
       return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './service-worker.js',
-        './icon-192.png',
-        './icon-512.png'
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
